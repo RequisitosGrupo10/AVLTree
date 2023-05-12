ac24b2dd41cce7f091669f5d8d98cbfb9505d174
José Manuel <jmsanchezrico@gmail.com>
2023-05-12 09:57:06 +0200

    ✅ Test adapted to JUnit5

--- src/test/java/avl/AvlNodeTest.java
+++ src/test/java/avl/AvlNodeTest.java
@@ -1,50 +1,187 @@
 package avl;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.*;
 
 /**
  * Created with IntelliJ IDEA. User: Antonio J. Nebro Date: 09/07/13 Time: 15:29
  */
 public class AvlNodeTest {
+  @Nested
+  @DisplayName("Given an empty node")
+  class EmptyNodeTest {
+    private static AvlNode<Object> emptyNode;
+    private static Object item;
 
-  private AvlNode<Integer> node;
+    @BeforeAll
+    static void setUp() throws Exception {
+      item = 5;
+      emptyNode = new AvlNode<>(item);
+    }
 
-  @Before
-  public void setUp() throws Exception {
-    node = new AvlNode<Integer>(5);
+
+    @AfterAll
+    public static void tearDown() throws Exception {
+      emptyNode = null;
+    }
+    @Test
+    @DisplayName("Has no left node")
+    public void Given_AnEmptyNode_When_AskingIfItHasALeftHasLeftChild_Then_ReturnsFalse() {
+      Assertions.assertFalse(emptyNode.hasLeft());
+    }
+
+    @Test
+    @DisplayName("Has no right node")
+    public void Given_AnEmptyNode_When_AskingIfItHasALeftHasRightChild_Then_ReturnsFalse() {
+      Assertions.assertFalse(emptyNode.hasRight());
+    }
+
+    @Test
+    @DisplayName("Has no right node")
+    public void Given_AnEmptyNode_When_AskingIfItHasAParent_Then_ReturnsFalse() {
+      Assertions.assertFalse(emptyNode.hasParent());
+    }
+
+    @Test
+    @DisplayName("Is a leaf")
+    public void Given_AnEmptyNode_When_AskingIfItIsALeaf_Then_ReturnsTrue() {
+      Assertions.assertTrue(emptyNode.isLeaf());
+    }
+
+    @Test
+    @DisplayName("Has no right node")
+    public void Given_AnEmptyNode_When_AskingIfItIseaf_Then_ReturnsFalse() {
+      int expectedValue = 0;
+      int actualValue =  emptyNode.getHeight();
+
+      Assertions.assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    @DisplayName("Right node is null")
+    public void Given_AnEmptyNode_When_GettingRightNode_Then_ReturnsNull() {
+      Assertions.assertNull(emptyNode.getRight());
+    }
+
+    @Test
+    @DisplayName("Left node is null")
+    public void Given_AnEmptyNode_When_GettingLeftNode_Then_ReturnsFalse() {
+      Assertions.assertNull(emptyNode.getLeft());
+    }
+
+    @Test
+    @DisplayName("Getting item returns the item passed on constructor")
+    public void Given_AnEmptyNode_When_GettingItem_Then_ReturnsItemOnConstruction() {
+      Object expectedItem = item;
+      Object actualItem = emptyNode.getItem();
+
+      Assertions.assertEquals(expectedItem, actualItem);
+    }
   }
 
-  @After
-  public void tearDown() throws Exception {
-    node = null;
+  @Nested
+  @DisplayName("Given a node with a left child")
+  class NodeWithLeftChildTest {
+    private static AvlNode<Object> nodeWithLeftChild;
+
+    @BeforeAll
+    static void setUp() throws Exception {
+      nodeWithLeftChild = new AvlNode<>(5);
+      AvlNode<Object> leftChild = new AvlNode<>(6);
+
+      nodeWithLeftChild.setLeft(leftChild);
+
+      leftChild.setParent(nodeWithLeftChild);
+      nodeWithLeftChild.updateHeight();
+    }
+
+
+    @AfterAll
+    public static void tearDown() throws Exception {
+      nodeWithLeftChild = null;
+    }
+
+    @Test
+    @DisplayName("Has only left child")
+    public void Given_ANodeWithOnlyALeftChild_When_AskingIfItOnlyHasOnlyLeftNode_Then_ReturnsTrue() {
+      Assertions.assertTrue(nodeWithLeftChild.hasOnlyALeftChild());
+    }
+
+    @Test
+    @DisplayName("Has only right child")
+    public void Given_ANodeWithOnlyARightChild_When_AskingIfItOnlyHasOnlyRightNode_Then_ReturnsFalse() {
+      Assertions.assertFalse(nodeWithLeftChild.hasOnlyARightChild());
+    }
+
   }
 
-  @Test
-  public void testHasLeft() {
-    assertFalse("testHasLeft", node.hasLeft());
-    AvlNode<Integer> node2 = new AvlNode<Integer>(6);
-    node.setLeft(node2);
-    assertTrue("testHasLeft", node.hasLeft());
+  @Nested
+  @DisplayName("Given a node with a right child")
+  class NodeWithRightChildTest {
+    private static AvlNode<Object> nodeWithRightChild;
+
+    @BeforeAll
+    static void setUp() throws Exception {
+      nodeWithRightChild = new AvlNode<>(5);
+      AvlNode<Object> rightChild = new AvlNode<>(7);
+
+      nodeWithRightChild.setRight(rightChild);
+
+      rightChild.setParent(nodeWithRightChild);
+      nodeWithRightChild.updateHeight();
+    }
+
+
+    @AfterAll
+    public static void tearDown() throws Exception {
+      nodeWithRightChild = null;
+    }
+
+    @Test
+    @DisplayName("Has only left child")
+    public void Given_ANodeWithOnlyARightChild_When_AskingIfItOnlyHasOnlyLeftNode_Then_ReturnsFalse() {
+      Assertions.assertFalse(nodeWithRightChild.hasOnlyALeftChild());
+    }
+
+    @Test
+    @DisplayName("Has only right child")
+    public void Given_ANodeWithOnlyARightChild_When_AskingIfItOnlyHasOnlyLeftNode_Then_ReturnsTrue() {
+      Assertions.assertTrue(nodeWithRightChild.hasOnlyARightChild());
+    }
   }
 
-  @Test
-  public void testHasRight() {
-    assertFalse("testHasRight", node.hasRight());
-    AvlNode<Integer> node2 = new AvlNode<Integer>(6);
-    node.setRight(node2);
-    assertTrue("testHasRight", node.hasRight());
-  }
+  @Nested
+  @DisplayName("Given a node with two children")
+  class NodeWithTwoChildrenTest {
+    private static AvlNode<Object> nodeWithTwoChildren;
 
-  @Test
-  public void shouldSetHeight() {
-    int expectedHeight = 1000213;
-    node.setHeight(expectedHeight);
-    assertEquals("Height is different from expected.", expectedHeight, node.getHeight());
+    @BeforeAll
+    static void setUp() throws Exception {
+      nodeWithTwoChildren = new AvlNode<>(5);
+      AvlNode<Object> leftChild = new AvlNode<>(6);
+      AvlNode<Object> rightChild = new AvlNode<>(7);
+
+      nodeWithTwoChildren.setLeft(leftChild);
+      nodeWithTwoChildren.setRight(rightChild);
+
+      leftChild.setParent(nodeWithTwoChildren);
+      rightChild.setParent(nodeWithTwoChildren);
+      nodeWithTwoChildren.updateHeight();
+    }
+
+
+    @AfterAll
+    public static void tearDown() throws Exception {
+      nodeWithTwoChildren = null;
+    }
+  }
+  @Nested
+  class AVLNodeGettersAndSettersTest {
+    // TODO - New Node from zero and check setters and getters
+    
+    // UpdateHeight
+    // GetHeight
+    // SetHeight
+    // SetItem
+    // SetParent
   }
 }
--- src/test/java/avl/AvlTreeTest.java
+++ src/test/java/avl/AvlTreeTest.java
@@ -1,14 +1,9 @@
 package avl;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNull;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
 import java.util.Comparator;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.*;
+
+import static org.springframework.test.util.AssertionErrors.*;
 
 /**
  * Created with IntelliJ IDEA. User: Antonio J. Nebro Date: 08/07/13
@@ -18,13 +13,13 @@ public class AvlTreeTest {
   AvlTree<Integer> avlTree;
   Comparator<?> comparator;
 
-  @Before
+  @BeforeEach
   public void setUp() throws Exception {
     comparator = Comparator.comparingInt((Integer o) -> o);
     avlTree = new AvlTree(comparator);
   }
 
-  @After
+  @AfterEach
   public void tearDown() throws Exception {
     avlTree = null;
     comparator = null;
